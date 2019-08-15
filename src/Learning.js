273973ac9365a13111cc7262f35bbedafa670661
olNYXlo <48833626+olNYXlo@users.noreply.github.com>
2019-08-15 16:08:27 +0800

    4.08pm   15/08/19   useState & callBack Examples

--- src/Learning.js
+++ src/Learning.js
@@ -103,19 +103,101 @@ const Arrow3 = ({mid}) => {
 // export default EX6;
 
 //=======================================================================================
-// EXAMPLE 7 - UseState (React Hooks)
+// EXAMPLE 7 - use of State Pre - ES6
 const EX7 = () => {
+    return <HeadlineEX7 />;
+};
+
+const HeadlineEX7 = () => {
+    const greeting = 'state - example';
+    return <h1>{greeting}</h1>;
+};
+
+// export default EX7;
+
+
+//=======================================================================================
+// EXAMPLE 8 - UseState (React Hooks)
+const EX8 = () => {
     return <HeadlineEX7 />;
 }
 
-const HeadlineEX7 = () => {
+const HeadlineEX8 = () => {
     const [greeting, setGreeting] = React.useState('NiHao');
     // takes in StateName & SetStateFunctionName
     return <h1>{greeting}</h1>;
 }
-export default EX7
+//export default EX8
 
 //=======================================================================================
+// EXAMPLE 9 - Event Handler that changes State
+const EX9 = () => {
+    return <HeadlineEX9 />;
+};
 
+const HeadlineEX9 = () => {
+    const [greeting, setGreeting] = React.useState('NiHao');
 
+return (
+
+    <div>
+        <h1>{greeting}</h1>
+        <input type = "text" value = {greeting} onChange ={event => setGreeting(event.target.value)} />;        
+    </div>
+    // prompts user to input any text
+    // any input text will be reflected on screen as the Header
+    // input text value (event.target.value) will be assigned to the Greeting state through the setGreeting method
+
+);
+};
+// export default EX9;
+//=======================================================================================
+// EXAMPLE 10 - Event Handler that changes State W better encapsulation
+const EX10 = () => {
+    return <HeadlineEX10 />;
+};
+
+const HeadlineEX10 = () => {
+    //HeadlineEX10 has state
+    const [greeting, setGreeting] = React.useState("NiHao");
+
+const handleChange = event => setGreeting(event.target.value);
+// event handler component
+// Do not use GetElementById in ReactJS. Is bad practice
+
+return (
+    <div>
+        <h1>{greeting}</h1>
+        <input type="text" value = {greeting} onChange={handleChange} />;
+    </div>
+);
+};
+// export default EX10;
+//=======================================================================================
+// EXAMPLE 11 - Callback function
+const EX11 = () => {
+    //EX11 has state while HeadlineEX11 has props passed to it from EX11
+    const [ greeting, setGreeting] = React.useState("NiHao");
+
+    const handleChange = event => setGreeting(event.target.value);
+
+    return (
+        <HeadlineEX11 headline = {greeting} onChangeHeadline = {handleChange} />
+        // HeadlineEX11 has 2 properties (props)
+        // headline with a value of greeting
+        // onChangeHeadline with a value of handleChange function
+    );
+};
+
+const HeadlineEX11 = ({headline, onChangeHeadline}) => (
+    // The function takes in 2 inputs, headline & onChangeHeadline
+    <div>
+        <h1>{headline}</h1>
+        <input type = "text" value = {headline} onChange = {onChangeHeadline} />
+    </div>
+    // outputs header with value of greeting
+    // with an input text box that on change will run the property onChangeHeadline
+    // which calls another function that handles the change
+);
+export default EX11;
 //=======================================================================================
